1d9d15be08cd4e0066db0742b8509391c5b72afa
Aon <66352312+AonBalouch@users.noreply.github.com>
2023-05-13 22:50:47 +0500

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { auth } from '../firebase'
+import bot from '../assets/logo2.png'
 import { FcGoogle } from 'react-icons/fc'
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
@@ -32,14 +33,20 @@ const SignIn = () => {
   }
 
   return (
+    
     <div className='signin'>
+      <img className='w-94 h-54' src={bot} alt="GPT" />
       <div className='signin__container'>
+
         <button className="signin__btn flex items-center justify-center" onClick={signInWithGoogle}>
           <FcGoogle className='m-2' />
           Sign in with Google
         </button>
 
       </div>
+      <p className='text-white mb-8 text-4xl font-bold'>
+      Welcome to Lattice AGI Chat
+        </p>
       <p className='signin__tos'>
         Do not violate the&nbsp;
         <a target="_blank" rel="noreferrer" href="https://openai.com/policies/usage-policies" className='font-bold'>
